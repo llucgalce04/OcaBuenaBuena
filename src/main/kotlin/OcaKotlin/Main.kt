4bcd864a70fecacf86a0075049e80bd4056dcb4e
llucgalce04 <llucgalce04@gmail.com>
2023-02-02 21:25:28 +0100

    Version Final del Juego :)

--- src/main/kotlin/OcaKotlin/Main.kt
+++ src/main/kotlin/OcaKotlin/Main.kt
@@ -5,8 +5,6 @@ fun main() {
     //Atribueix el nombre de jugadors
     val jugador1 = Jugador(Intro().retornarNombreval(), Intro().retornarColorJugador(),0,false,true, 1)
     val jugador2 = Jugador(Intro().retornarNombreval(), Intro().retornarColorJugador(),0,false,true, 1)
-
-
     val joc = OcaGame(listOf(jugador1,jugador2))
     joc.play()
 
